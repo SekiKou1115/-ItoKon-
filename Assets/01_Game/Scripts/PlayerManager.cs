ae1560e5b5fa8d959f5798fe51c42ce77e0c816a
KokiSekitsuka <raguna1115@gmail.com>
2024-07-01 16:11:48 +0900

    プレイヤー同士触れなくした

--- Assets/01_Game/Scripts/PlayerManager.cs
+++ Assets/01_Game/Scripts/PlayerManager.cs
@@ -102,6 +102,11 @@ public class PlayerManager : MonoBehaviour
 
         // 子オブジェクト取得
         _player = GetChildren(gameObject);
+        // プレイヤー同士の当たり判定を無視
+        Physics.IgnoreCollision(
+                _player[0].GetComponent<CapsuleCollider>(),
+                _player[1].GetComponent<CapsuleCollider>(),
+                true);
 
         // ライフ数リセット
         _life = _maxLife;
