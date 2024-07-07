14bb6b12b89459250de0796f208590e6d4449369
Sammy <kouyou.sbudiman@gmail.com>
2024-07-07 21:52:55 +0900

    落ちる岩スクリプト改良、リスポーンprefabとスクリプト追加

--- Assets/01_Game/Scripts/FallingPlatform.cs
+++ Assets/01_Game/Scripts/FallingPlatform.cs
@@ -1,37 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-    [SerializeField] private float fallDelay; // Delay 
-    [SerializeField] private float destroyDelay = 2f; //Destroy  
+    [SerializeField] private float fallDelay = 1f; // Delay 時間
+    [SerializeField] private float destroyDelay = 2f; // Destroy 時間
     private Rigidbody rb;
     private bool hasFallen = false;
+    private ObjectRespawn _respawner;
+    private Vector3 spawnPosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        _respawner = FindObjectOfType<ObjectRespawn>();
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player") && !hasFallen)
         {
+            spawnPosition = transform.position; // 現在のPosを保持
             Invoke("Fall", fallDelay);
-            Invoke("DestroyPlatform", destroyDelay);
+            Invoke("DestroyPlatform", fallDelay + destroyDelay);
             hasFallen = true;
         }
     }
 
     void Fall()
     {
-        rb.isKinematic = false; // Enable gravity
+        rb.isKinematic = false; 
     }
 
     void DestroyPlatform()
     {
-        Destroy(gameObject);  // Destroy the platform GameObject
+       
+        
+        if (_respawner != null)
+        {
+            _respawner.RespawnPlatform(spawnPosition); // respawn managerの関数を呼び出す
+        }
+        Destroy(gameObject); // Destroy 
     }
 }
-    
