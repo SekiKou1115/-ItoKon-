af3360e7d96a72e5593dccce2b515d6481a3bfc3
KokiSekitsuka <raguna1115@gmail.com>
2024-07-17 13:53:11 +0900

    岩や地形の見た目

---
+++ Assets/Low Poly Package/Editor/Low Poly Package/WaterBuilderEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(WaterBuilder))]
+public class WaterBuilderEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		WaterBuilder script = (WaterBuilder)target;
+		if(GUILayout.Button("Build Water"))
+		{
+			script.BuildWater();
+		}
+	}
+}
